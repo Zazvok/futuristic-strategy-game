214f46ed1d55a83c423313fe5d21974be52bbdcf
Zazvok <zalanocsai@gmail.com>
2024-04-08 20:21:46 +0200

    tweaking in character.gd

--- scenes/character.gd
+++ scenes/character.gd
@@ -1,8 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 500.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 
@@ -20,11 +19,11 @@ func _physics_process(delta):
 	if directionX:
 		velocity.x = directionX * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, 12)
+		velocity.x = move_toward(velocity.x, 0, 11)
 	var directionY = Input.get_axis("moveUp","moveDown")
 	if directionY:
 		velocity.y = directionY * SPEED
 	else:
-		velocity.y = move_toward(velocity.y,0,12)
+		velocity.y = move_toward(velocity.y,0,11)
 
 	move_and_slide()
