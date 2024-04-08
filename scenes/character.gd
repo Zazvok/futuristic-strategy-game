0af292f0482f86cf59e9578e4ac4b42a29818fc6
Zazvok <zalanocsai@gmail.com>
2024-04-08 20:18:02 +0200

    camera movement

--- scenes/character.gd
+++ scenes/character.gd
@@ -2,27 +2,29 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -700.0
+const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("moveLeft", "moveRight")
-	if direction:
-		velocity.x = direction * SPEED
+	var directionX = Input.get_axis("moveLeft", "moveRight")
+	if directionX:
+		velocity.x = directionX * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, 10)
+		velocity.x = move_toward(velocity.x, 0, 12)
+	var directionY = Input.get_axis("moveUp","moveDown")
+	if directionY:
+		velocity.y = directionY * SPEED
+	else:
+		velocity.y = move_toward(velocity.y,0,12)
 
 	move_and_slide()
