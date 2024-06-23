b60398e209bf892f9fecbbf38d8d139910fd246d
Zazvok <zalanocsai@gmail.com>
2024-06-23 19:35:09 +0200

    grid placement prototype finished

--- zazvok/TileMap.gd
+++ zazvok/TileMap.gd
@@ -11,3 +11,11 @@ func _ready():
 				"Type": "Grnd"
 			}
 			set_cell(0, Vector2(x,y), 3, Vector2(0,0),0)
+	print(dic)
+func _process(delta):
+	var tile = local_to_map(get_global_mouse_position())
+	for x in gridsize:
+		for y in gridsize:
+			erase_cell(1, Vector2(x,y))
+	if dic.has(str(tile)):
+		set_cell(1, tile, 0,Vector2i(0,0),0)
