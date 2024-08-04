5ec8150434a5f1240ba98fcff237884e957ccf35
Zazvok <zalanocsai@gmail.com>
2024-08-04 11:43:32 +0200

    cleaned up files

--- zazvok/skyscrapermovement.gd
+++
@@ -1,8 +0,0 @@
-extends CharacterBody2D
-var skyscraper1 = preload("res://zazvok/skyscraper.tscn")
-var inst = skyscraper1.instantiate()
-
-func _process(delta):
-	if GlobalVar.move == true:
-		pass
-		
