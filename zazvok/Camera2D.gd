3fafc7ce695bbf147a55420a1bfd0b750764a97f
Zazvok <zalanocsai@gmail.com>
2024-10-19 19:31:58 +0200

    forest generation

--- zazvok/Camera2D.gd
+++ zazvok/Camera2D.gd
@@ -9,8 +9,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_key_pressed(KEY_UP):
-		zoom.x += 0.1
-		zoom.y += 0.1
+		zoom.x += 0.01
+		zoom.y += 0.01
 	elif Input.is_key_pressed(KEY_DOWN):
-		zoom.x -= 0.1
-		zoom.y -= 0.1
+		zoom.x -= 0.01
+		zoom.y -= 0.01
--- zazvok/TileMap.gd
+++ zazvok/TileMap.gd
@@ -1,10 +1,12 @@
 extends TileMap
 
 var ran = RandomNumberGenerator.new()
-var gridsize = 16
+var gridsize = 50
 var dic = {}
 var dicfa = {}
-
+var fasokasag = 0
+var x = 0
+var falist = []
 func _ready():
 	for x in gridsize:
 		for y in gridsize:
@@ -12,12 +14,39 @@ func _ready():
 				"Type": "Grnd"
 			}
 			set_cell(0, Vector2(x,y), 3, Vector2(0,0),0)
-			var skibidi = ran.randf_range(0,2)
-			if skibidi > 1.5:
+			
+			
+			
+			var skibidi = ran.randf_range(0,6)
+			var tile2 = local_to_map(get_global_mouse_position())
+			if skibidi >5.99:
 				dicfa[str(Vector2(x,y))] = {
 					"type": "fa"
 				}
 				set_cell(1,Vector2(x,y),2,Vector2(0,0),0)
+				fasokasag += 1
+				
+	
+	for k in 20:
+		for xx in gridsize:
+			for yy in gridsize:
+				if(get_cell_source_id(1,Vector2(xx,yy)) == 2):
+					var sigma = ran.randf_range(0,4)
+					if sigma <= 1:
+						falist.insert(0,Vector2(xx,yy+1))
+					elif sigma <= 2 and sigma >= 1:
+						falist.insert(0,Vector2(xx,yy-1))
+					elif sigma <= 3 and sigma >= 2:
+						falist.insert(0,Vector2(xx-1,yy))
+					elif sigma <= 4 and sigma >= 3:
+						falist.insert(0,Vector2(xx+1,yy))
+				
+				
+	#lerakja
+		for i in len(falist):
+			set_cell(1,falist[i],2,Vector2(0,0),0)
+		
+		
 	print(dic)
 func _process(delta):
 	var tile = local_to_map(get_global_mouse_position())
@@ -36,3 +65,9 @@ func _process(delta):
 		if Input.is_key_pressed(KEY_O):
 			erase_cell(1,tile)
 			erase_cell(2,Vector2(tile[0],tile[1]-1))
+	if Input.is_key_pressed(KEY_R):
+		for x in 55:
+			for y in 55:
+				erase_cell(1,Vector2(x,y))
+	if Input.is_key_pressed(KEY_E):
+		_ready()
