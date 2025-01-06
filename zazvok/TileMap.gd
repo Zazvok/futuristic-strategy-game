16d7c754ba18d7fa8dea5b73a4c5e747536eb743
Zazvok <zalanocsai@gmail.com>
2025-01-06 17:20:42 +0100

    restarted from scratch, again

--- zazvok/TileMap.gd
+++ zazvok/TileMap.gd
@@ -1,52 +1,12 @@
 extends TileMap
 
 var ran = RandomNumberGenerator.new()
-var gridsize = 50
-var dic = {}
 var dicfa = {}
 var fasokasag = 0
 var x = 0
 var falist = []
-var mountainlist = []
+var gridsize = 50
 func _ready():
-	for x in gridsize:
-		for y in gridsize:
-			dic[str(Vector2(x,y))] = {
-				"Type": "Grnd"
-			}
-			set_cell(0, Vector2(x,y), 7, Vector2(0,0),0)
-	erdo()
-	alt()
-	print(mountainlist)
-		
-	print(dic)
-
-func _process(delta):
-	var tile = local_to_map(get_global_mouse_position())
-	for x in gridsize:
-		for y in gridsize:
-			erase_cell(3, Vector2(x,y))
-	if dic.has(str(tile)):
-		set_cell(3, tile, 0,Vector2i(0,0),0)
-		if Input.is_key_pressed(KEY_P):
-			if get_cell_source_id(1,tile) == 2:
-				print("nope")
-				return
-			set_cell(1,tile,5,Vector2i(0,0),0)
-			set_cell(2,Vector2(tile[0],tile[1]-1),6,Vector2i(0,0),0)
-		if Input.is_key_pressed(KEY_O):
-			erase_cell(1,tile)
-			erase_cell(2,Vector2(tile[0],tile[1]-1))
-		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-			print(dic[str(tile)])
-	if Input.is_key_pressed(KEY_R):
-		for x in 55:
-			for y in 55:
-				erase_cell(1,Vector2(x,y))
-	if Input.is_key_pressed(KEY_E):
-		_ready()
-
-func erdo():
 	for x in gridsize:
 		for y in gridsize:		
 			var skibidi = ran.randf_range(0,6)
@@ -56,7 +16,7 @@ func erdo():
 				}
 				set_cell(1,Vector2(x,y),2,Vector2(0,0),0)
 				fasokasag += 1
-				
+
 	for k in 30:
 		for xx in gridsize:
 			for yy in gridsize:
@@ -73,34 +33,36 @@ func erdo():
 				
 				
 		for i in len(falist):
+			dicfa[str(falist[i])] = {
+					"type": "fa"
+				}
 			set_cell(1,falist[i],2,Vector2(0,0),0)
-
-func alt():
-	for x in gridsize:
-		for y in gridsize:
-			var rand1 = ran.randf_range(0,500)
-			if rand1 >= 499:
-				set_cell(0,Vector2(x,y),4)
+	print(dicfa)
+func _process(delta):
 	
-	for x in gridsize:
-		for y in gridsize:
-			mountainlist.insert(0,get_surrounding_cells(Vector2(x,y)))
 			
-	#for k in len(mountainlist):
-		#set_cell(0,Vector2(mountainlist[k]),3,Vector2(0,0),0)
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	var tile = local_to_map(get_global_mouse_position())
+	for x in gridsize:
+		for y in gridsize:
+			erase_cell(3, Vector2(x,y))
+	if dicfa.has(str(tile)):
+		set_cell(3, tile,0)
+		if Input.is_key_pressed(KEY_P):
+			if get_cell_source_id(1,tile) == 2:
+				print("nope")
+				return
+			set_cell(1,tile,5,Vector2i(0,0),0)
+			set_cell(2,Vector2(tile[0],tile[1]-1),6,Vector2i(0,0),0)
+		if Input.is_key_pressed(KEY_O):
+			erase_cell(1,tile)
+			erase_cell(2,Vector2(tile[0],tile[1]-1))
+		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			print(dicfa[str(tile)])
+	if Input.is_key_pressed(KEY_R):
+		for x in 55:
+			for y in 55:
+				erase_cell(1,Vector2(x,y))
+	if Input.is_key_pressed(KEY_E):
+		_ready()
+
+
