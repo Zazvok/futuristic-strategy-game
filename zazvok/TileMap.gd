2f86288917f762156f562030b06f56b158f655ed
Zazvok <zalanocsai@gmail.com>
2025-07-24 22:26:09 +0200

    spawning forests outside of the matrix

--- zazvok/TileMap.gd
+++ zazvok/TileMap.gd
@@ -7,6 +7,10 @@ var x = 0
 var falist = []
 var gridsize = 50
 func _ready():
+	spawn()
+	
+	
+	'''
 	for x in gridsize:
 		for y in gridsize:		
 			var skibidi = ran.randf_range(0,6)
@@ -14,8 +18,18 @@ func _ready():
 				dicfa[str(Vector2(x,y))] = {
 					"type": "fa"
 				}
-				set_cell(1,Vector2(x,y),2,Vector2(0,0),0)
-				fasokasag += 1
+				'''
+
+
+
+
+
+
+
+
+
+func spawn():
+	set_cell(1,Vector2(25,25),2,Vector2(0,0),0)
 
 	for k in 30:
 		for xx in gridsize:
@@ -37,16 +51,21 @@ func _ready():
 					"type": "fa"
 				}
 			set_cell(1,falist[i],2,Vector2(0,0),0)
-	print(dicfa)
-func _process(delta):
+	#print(dicfa)
 	
-			
+	
+	
+	
+	
+func _process(delta):
 	var tile = local_to_map(get_global_mouse_position())
-	for x in gridsize:
-		for y in gridsize:
+	Zazvoktest.mousepos = local_to_map(Vector2(0,0))
+	print(Zazvoktest.mousepos)
+	for x in range(-1,51):
+		for y in range(-1,51):
 			erase_cell(3, Vector2(x,y))
 	if dicfa.has(str(tile)):
-		set_cell(3, tile,0)
+		set_cell(3, tile,0,Vector2i(0,0),0)
 		if Input.is_key_pressed(KEY_P):
 			if get_cell_source_id(1,tile) == 2:
 				print("nope")
@@ -58,11 +77,5 @@ func _process(delta):
 			erase_cell(2,Vector2(tile[0],tile[1]-1))
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 			print(dicfa[str(tile)])
-	if Input.is_key_pressed(KEY_R):
-		for x in 55:
-			for y in 55:
-				erase_cell(1,Vector2(x,y))
-	if Input.is_key_pressed(KEY_E):
-		_ready()
 
 
--- zazvok/zazvoktest.gd
+++ zazvok/zazvoktest.gd
@@ -1,1 +1,18 @@
 extends Node2D
+
+var forestload = preload("res://zazvok/forest.tscn")
+var sigma = Vector2(100,100)
+var mousepos
+
+func _process(delta):
+	mousepos = get_global_mouse_position()
+
+func _input(event):
+	if Input.is_key_pressed(KEY_E):
+		forestgen(sigma)
+
+
+func forestgen(pos):
+	var forestinst = forestload.instantiate()
+	forestinst.position = pos
+	add_child(forestinst)
